c6bc18301483720faf057064303dcf75587a5429
Al Camara <Alhousseinec@gmail.com>
2022-08-26 23:53:25 -0500

    finish

--- client.js
+++ client.js
@@ -1,6 +1,29 @@
+
+
 $(document).ready(readyNow);
 
 
 function readyNow() {
-    console.log('test');
+     $('.loading-test').on('click', unblurImage)
 }
+
+function unblurImage() {
+    let loading = 0;
+    var interval = setInterval(function(){
+        if(loading > 98) clearInterval(interval);
+        loading++;
+        console.log(loading);
+        $('.loading-test').html(`${loading}%`).css({
+            opacity: scale(loading,0,100, 1, 0)
+        })
+        $('.bg').css({
+            filter: `blur(${scale(loading,0,100, 30, 0)}px)`
+        })
+    },30)
+}
+
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
